092f2063a4c957b83eea7d2631fc3fa195265372
Alexander Kuznetsov <geff21st@gmail.com>
2022-01-25 23:08:00 +0300

    fix viewport init

--- script.js
+++ script.js
@@ -12,14 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const initViewPort = () => {
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-      // We execute the same script as before
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
   initViewPort();
+  // We listen to the resize event
+  window.addEventListener('resize', initViewPort);
 
   const initCardIds = () => {
     document.querySelectorAll('.card').forEach($el => {
